681d24b383f4165c6a2697c3034e1520f029daae
NoSignificantWorries <dmitry.a.karpachev@gmail.com>
2026-03-16 15:33:39 +0700

    LLM work started
    Created basic LLM module for messages classification.
    Added key dependencies for LLM worflow.

--- worker/main.py
+++ worker/main.py
@@ -1,6 +1,30 @@
-def main():
-    print("Hello from worker!")
+import json
+from pathlib import Path
 
+# custom modules
+from llm import LLM
+
+
+class Table:
+    def __init__(self, data) -> None:
+        self.parse(data)
+
+    def parse(self, data_dict):
+        for key, val in data_dict.items():
+            setattr(self, key, val)
+
+
+def read_config(path: Path):
+    with open(path, "r") as config_file:
+        config_data = json.load(config_file)
+
+    llm_config = Table(config_data["llm"])
+
+    return (llm_config,)
+
+
+def main():
+    ...
 
 if __name__ == "__main__":
     main()
